ba33f306f1cf7ba5a8f67ed418114524b74e3d69
Elisa <elisath01@gmail.com>
2023-07-14 23:40:09 -0300

    API de Libros en Node.js con Express.js, Mongoose, MongoDB. Aun sin Autenticacion y Autorizacion.

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,17 @@
+const errorHandler = (err, req, res, next) => {
+    //Verificar si el error tiene un codigo de estado definido, de lo contrario, establecer el codigo de estado.
+    const statusCode = err.statusCode || 500;
+
+    //Construir objeto de respuesta de error
+    const errorResponse = {
+        error: {
+            message: err.message || "Error interno del servidor",
+            code: err.code || "internal_error",
+        },
+    };
+
+    //Enviar respuesta de error en formato JSON
+    res.status(statusCode).json(errorResponse);
+};
+
+module.exports = errorHandler;
